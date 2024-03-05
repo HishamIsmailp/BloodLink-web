e69853876caef62487afc901c7c3df7e26c41774
Hisham <hishammuhammed.in@gmail.com>
2024-03-05 22:11:15 +0530

    created nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Image from 'next/image';
+
+import Banner from '../utils/images/banner.png';
 
 export default function Home() {
   return (
-    <div></div>
+    <div style={{backgroundImage: Banner.src, backgroundSize: "cover" }}>
+      <div className='min-vh-100 w-100'>pgfghjk</div>
+    </div>
   );
 }
