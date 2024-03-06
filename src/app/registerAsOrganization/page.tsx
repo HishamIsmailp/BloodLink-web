616c1587b88c2de81e97f00fa95689ebf44b49b2
Hisham <hishammuhammed.in@gmail.com>
2024-03-06 16:02:17 +0530

    added nav bar in register as organaization page

---
+++ src/app/registerAsOrganization/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+export default function registerAsOrganization() {
+  return (
+    <>
+    <div className="p-5 m-2 mt-1 position-fixed top-0 w-100"><NavBar /></div>
+        <div>registerAsOrganization</div>
+    </>
+  )
+}
